56a7c1350ebc2a1116b6425813f65ddd05907264
ShivP <ShivP210>
2019-09-25 00:03:20 +0530

    First commiit

---
+++ Models/Tables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tables.Models
+{
+    public class Tables
+    {
+       
+        public void TableCalculation(int multiplier, int multiplicant)
+        {
+            
+                for (int j = 1; j < multiplicant+1; j++)
+                {
+                       
+                }
+
+        }
+    }
+}
